94c6975816bc040964005dc27b5770f22997aa14
Bahattin Cinic <bahattincinic@gmail.com>
2014-07-31 23:15:12 +0300

    add form types

--- angularjs-form-builder.js
+++ angularjs-form-builder.js
@@ -5,41 +5,47 @@ angular.module('formBuilder', []);
 angular.module('formBuilder').directive('ngForm', function(){
     return {
         restrict: 'E',
-        scope: {
-            form: '=form'
-        },
-        templateUrl: 'template/form/form.html'
+        scope: { form: '=' },
+        templateUrl: 'template/form/form.html',
     };
 });
 
 angular.module('formBuilder').directive('ngField', function(){
     return {
         restrict: 'E',
-        scope: {
-            field: '=field'
-        },
-        link: function(scope, element, attrs) {
-           scope.getContentUrl = function() {
-                return 'template/form/fields/' + attrs.type + '.html';
-           }
-       },
-       template: '<div ng-include="getContentUrl()"></div>'
+        scope: { field: '=' },
+        template: '<div ng-include="\'template/form/fields/\' + field.type + \'.html\'"></div>'
     };
 });
 
 angular.module('formBuilder').run(["$templateCache", function($templateCache) {
   $templateCache.put("template/form/form.html",
-    "<form ng-submit=\"form.submit\">\n"+
-    "<ng-field ng-repeat=\"field in form.attributes\" field=\"field\"></ng-field>"+
+    "<form ng-submit=\"form.submit()\" role=\"form\">\n"+
+    "<ng-field ng-repeat=\"field in form.attributes\" data-field=\"field\"></ng-field>"+
     "</form>");
 }]);
 
 angular.module('formBuilder').run(["$templateCache", function($templateCache) {
-    $templateCache.put("template/form/fields/texfield.html",
-        "<div class=\"field row\">\n" +
-        "<div class=\"span2\" ng-bind=\"field.title\"></div>\n" +
+    $templateCache.put("template/form/fields/textfield.html",
+        "<div class=\"form-group\">\n" +
+        "<label for=\"{{field.title}}\" ng-bind=\"field.title\"></label>\n" +
         "<div class=\"span4\">\n" +
-        "<input type=\"text\" ng-model=\"field.value\" ng-value=\"field.value\" ng-required=\"field.required\">\n" +
+        "<input type=\"text\" ng-model=\"field.value\" id=\"{{field.title}}\" class=\"form-control\" ng-value=\"field.value\" ng-required=\"field.required\">\n" +
         "<span ng-show=\"field.required && !field.value\">* required</span>\n" +
         "</div> </div>");
+}]);
+
+angular.module('formBuilder').run(["$templateCache", function($templateCache) {
+    $templateCache.put("template/form/fields/email.html",
+        "<div class=\"form-group\">\n" +
+        "<label for=\"{{field.title}}\" ng-bind=\"field.title\"></label>\n" +
+        "<div class=\"span4\">\n" +
+        "<input type=\"email\" ng-model=\"field.value\" id=\"{{field.title}}\" class=\"form-control\" ng-value=\"field.value\" ng-required=\"field.required\">\n" +
+        "<span ng-show=\"field.required && !field.value\">* required</span>\n" +
+        "</div> </div>");
+}]);
+
+angular.module('formBuilder').run(["$templateCache", function($templateCache) {
+    $templateCache.put("template/form/fields/submit.html",
+        "<input type=\"submit\" class=\"btn btn-primary\" ng-value=\"field.value\">\n");
 }]);
